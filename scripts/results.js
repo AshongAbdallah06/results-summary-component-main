976d76928b3e31cf187c97c47534b0f5ecf39aea
AshongAbdallah06 <ashongabdalla15@gmail.com>
2023-11-06 22:19:54 +0000

    Seperate code in right side cpntainer into modules

---
+++ scripts/results.js
@@ -0,0 +1,21 @@
+export const results = [{
+    img: "./assets/images/icon-reaction.svg",
+    name: "Reaction",
+    percent: "80",
+    class: "reaction"
+}, {
+    img: "./assets/images/icon-memory.svg",
+    name: "Memory",
+    percent: "92",
+    class: "memory"
+}, {
+    img: "./assets/images/icon-verbal.svg",
+    name: "Verbal",
+    percent: "61",
+    class: "verbal"
+}, {
+    img: "./assets/images/icon-visual.svg",
+    name: "Visual",
+    percent: "72",
+    class: "visual"
+}];
--- scripts/summary.js
+++ scripts/summary.js
@@ -1,24 +1,4 @@
-const results = [{
-    img: "./assets/images/icon-reaction.svg",
-    name: "Reaction",
-    percent: "80",
-    class: "reaction"
-}, {
-    img: "./assets/images/icon-memory.svg",
-    name: "Memory",
-    percent: "92",
-    class: "memory"
-}, {
-    img: "./assets/images/icon-verbal.svg",
-    name: "Verbal",
-    percent: "61",
-    class: "verbal"
-}, {
-    img: "./assets/images/icon-visual.svg",
-    name: "Visual",
-    percent: "72",
-    class: "visual"
-}];
+import { results } from "./results.js";
 
 let resultsHTML = "";
 results.forEach((result) => {
