63cbadcc78431b0ae379b4d24c140f376925bf69
AshongAbdallah06 <ashongabdalla15@gmail.com>
2023-11-06 22:13:26 +0000

    Style the right side container

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const results = [{
     img: "./assets/images/icon-reaction.svg",
     name: "Reaction",
-    percent: "80 / 100",
+    percent: "80",
     class: "reaction"
 }, {
     img: "./assets/images/icon-memory.svg",
     name: "Memory",
-    percent: "92 / 100",
+    percent: "92",
     class: "memory"
 }, {
     img: "./assets/images/icon-verbal.svg",
     name: "Verbal",
-    percent: "61 / 100",
+    percent: "61",
     class: "verbal"
 }, {
     img: "./assets/images/icon-visual.svg",
     name: "Visual",
-    percent: "72 / 100",
+    percent: "72",
     class: "visual"
 }];
 
 let resultsHTML = "";
 results.forEach((result) => {
     resultsHTML += `
-        <div>
+    <div class="container ${(result.name).toLowerCase()}-container">
+        <div class="icons">
             <img src="${result.img}">
-            <p class="${result.class}">${result.name}</p>
-            <span>${result.percent}</span>
         </div>
+        <div class="${(result.name).toLowerCase()}">
+            ${result.name}
+        </div>
+        <div class="hundred-percent">
+            <p class="percent">${result.percent} <span class="hundred">/ 100</span></p>
+        </div>
+        
+    </div>
     `
 })
 
